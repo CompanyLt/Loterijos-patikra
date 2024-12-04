1359e5fea9544a5c39b4a0b8392916ace120727a
CompanyLt <145833655+CompanyLt@users.noreply.github.com>
2024-12-04 20:55:28 +0200

    LotteryPerlas.java

--- Loterijos patikra/src/LotteryPerlas.java
+++ Loterijos patikra/src/LotteryPerlas.java
@@ -25,7 +25,7 @@ public class LotteryPerlas extends Navigation
             System.out.println("Pasirinkite veiksma");
             if(!scanner.hasNextInt()) {
                 System.out.println("Klaida! Prašome įvesti tik skaičių.");
-                scanner.next(); // Išvalome blogą įvestį
+                scanner.next(); 
                 continue;
             }
             int command = scanner.nextInt();
